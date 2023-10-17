1dae14c103f6e1061ebece2676616373a8b2c435
senseiX21 <mahdix2118@protonmail.com>
2023-10-17 17:06:31 +0100

    Initialized the project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import Navbar from '@/components/ui/navbar'
+import Footer from '@/components/ui/footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,8 +17,12 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <div data-theme='garden'>
+      <Navbar />
+      <div className='h-screen'>
+        {children}
+      </div>
+      <Footer />
+    </div>
   )
 }
