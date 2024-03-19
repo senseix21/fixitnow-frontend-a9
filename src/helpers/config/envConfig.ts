f58005b3e86330d795e80966017d9283e1d87605
senseiX21 <mahdix2118@protonmail.com>
2024-03-19 16:27:06 +0600

    up

--- src/helpers/config/envConfig.ts
+++ src/helpers/config/envConfig.ts
@@ -1,3 +1,3 @@
 export const getBaseUrl = (): string => {
-    return process.env.NEXT_PUBLIC_API_BASE_URL as string || "http://localhost:5000/api/v1";
+    return process.env.url || "https://fixitnow-backend-a9.vercel.app/api/v1" as string;
 }
